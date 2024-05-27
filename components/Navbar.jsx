7d97e1ba73ecd2a5c339734c7b0cfcd38c9ea80a
Aman Raj <amanraj33039@gmail.com>
2024-05-27 18:13:55 +0000

    added images directory in pubic

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return (
         <nav>
-            <img src="../globe.svg" className="nav--svg" alt="Globe"/>
+            <img src="../images/globe.svg" className="nav--svg" alt="Globe"/>
             <div className="nav--header"> my travel journal </div>
         </nav>
     )
